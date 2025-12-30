deb7e58ae2c6fc4cc5eb275acfa05adaac10ad5f
mohya972 <mohya972n@gmail.com>
2025-12-30 08:51:20 -0400

    Model Post et sa migration

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    // on utilise les factories pour les tests et le seeding
+    use HasFactory;
+
+    // on sécurise les champs modifiables en masse
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id',
+        'published'
+    ];
+
+    // on précise les types de certains champs
+    protected $casts = [
+        'published' => 'boolean',
+    ];
 }
